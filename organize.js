b926b708fb230aa3344457882e9d643b8f9dab55
ColorfulCodes <codescolorful@gmail.com>
2019-01-29 10:23:28 -0500

    My first commit and exercise from exercises for
    programmers.

---
+++ organize.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+let listOfNames =fs.readFileSync('names.txt', 'utf8')
+
+function alp (contents) {
+	let a = contents.split("\n").sort();
+	let b = a.join("\n");
+	console.log('Total of ' + a.length + " names");
+	console.log("-----------------------------");
+	return b
+		// let count=num of names;
+	//	console.log('Total of ' + count + " names");
+	//	console.log('--------------------');
+		//split by each new line into an array
+	//use sort method on array
+	//join array by new lines
+};
+console.log(alp(listOfNames));
